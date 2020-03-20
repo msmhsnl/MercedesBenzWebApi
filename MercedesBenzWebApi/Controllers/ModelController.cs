879f7f6421eccc5c6773458b53c1c541ab019ed9
msmhsnl <msmhsnl@gmail.com>
2020-03-20 05:43:24 +0300

    Project Upload

---
+++ MercedesBenzWebApi/Controllers/ModelController.cs
@@ -0,0 +1,51 @@
+﻿using MercedesBenzWebApi.Models.DTO;
+using MercedesBenzWebApi.Models.ORM;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace MercedesBenzWebApi.Controllers
+{
+    public class ModelController : ApiController
+    {
+        MercedesBenzEntities db = new MercedesBenzEntities();
+
+        
+
+        public List<ProductModel> GetProductsByModel(string model)
+        {
+            if(model == "all"){
+                List<ProductModel> ProductList = db.Products.Select(a => new ProductModel()
+                {
+                    Model = a.Model,
+                    Name = a.Name,
+                    Slogan = a.Slogan,
+                    IsNew = a.IsNew,
+                    Discountable = a.Discountable,
+                    ImgSrc = a.ImgSrc
+                }).ToList();
+
+                return ProductList;
+            }
+            else
+            {
+                List<ProductModel> ProductList = db.Products.Where(a => a.Model == model)
+                .Select(a => new ProductModel()
+                {
+                    Model = a.Model,
+                    Name = a.Name,
+                    Slogan = a.Slogan,
+                    IsNew = a.IsNew,
+                    Discountable = a.Discountable,
+                    ImgSrc = a.ImgSrc
+                }).ToList();
+
+                return ProductList;
+            }
+            
+        }
+    }
+}
---
+++ MercedesBenzWebApi/Models/DTO/ProductModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MercedesBenzWebApi.Models.DTO
+{
+    public class ProductModel
+    {
+        public string Model { get; set; }
+        public string Name { get; set; }
+        public string Slogan { get; set; }
+        public bool? IsNew { get; set; }
+        public bool? Discountable { get; set; }
+        public string ImgSrc { get; set; }
+    }
+}
